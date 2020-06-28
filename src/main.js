0080dddbf887599e7d5a90919edd3cdf25eba80d
hiroaki.murayama <hotsoup.h@gmail.com>
2020-06-29 01:36:15 +0900

    Add poker model and showing table

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
-
+const router = new VueRouter({
+  mode: 'history',
+  routes: [{
+    path: '/',
+    component: App
+  }, ]
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router
+}).$mount('#app');
